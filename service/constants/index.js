2528bbfb39443f32a1083eec8dcea0ad84d11b46
krkhunti <krkhunti.elance@gmail.com>
2018-04-28 11:48:15 +0530

    Added dropdown options

--- service/constants/index.js
+++ service/constants/index.js
@@ -1,50 +1,70 @@
 global.inspectionTypes = {
-  "VPPI" : "Vessel Pre-purchase Inspection",
-  "VCA": "Vessel Condition Assessment",
-  "DDM": "Dry-Dock Management",
-  "SOI": "Ship Owner Inspection",
-  "PCI": "Pre-Charter Inspection",
-  "RS": "Repair Supervision",
-  "PVI": "Pre-Vetting Inspection",
-  "ISMIA": "ISM Internal Audit",
-  "ISMTI": "ISM Technical Inspection",
-  "CS": "Cargo Survey",
-  "DS": "Damage Survey",
-  "WS": "Warranty Survey",
-  "PICI": "P&I Condition Inspection",
-  "BS": "Bunker Survey",
-  "OHCS": "On Hire & Off-hire Condition Survey",
-  "RDI": "Re-delivery Inspection"
+  "BS" : "Bunkery Survey",
+  "CLS" : "Cargo Loading Survey",
+  "DFI" : "Dead Frieght Investigation",
+  "DDPS" : "Dry-Dock Project Supervision",
+  "FSI" : "Flag State Inspection",
+  "HMCI" : "H&M Insurance Claim Inspection",
+  "ISMA" : "ISM Audits",
+  "ISPSMLCA" : "ISPS/MLC Audits",
+  "MCI" : "Marine Casualty Investigation",
+  "MARPOLI" : "MARPOL Inspection",
+  "NA" : "Navigation Audits",
+  "PICI" : "P&I Condition Inspection",
+  "PortCaptaincy" : "Port Captaincy",
+  "PDI" : "Pre-docking Inspection",
+  "PPI" : "Pre-Purchase Inspection",
+  "PVI" : "Pre-Vetting Inspection",
+  "SAI" : "Ship Accommodation Inspection",
+  "STSMO" : "STS  Mooring Operation",
+  "VCI" : "Vessel Condition Inspection",
+  "WCI" : "Warranty Claim Inspection"
+};
+
+global.inspectorTitle = {
+  "BS" : "Bunker Surveyor ",
+  "CS" : "Cargo  Survyor ",
+  "CHI" : "Cargo Holds Inspector",
+  "CDIVI" : "CDI Vetting Inspector",
+  "FSI" : "Flag State Inspector",
+  "MAI" : "Marine Accident Investigator",
+  "MA" : "Marine Arbitrator",
+  "MEA" : "Marine Environmental Auditor",
+  "MIA" : "Marine Insurance Adjustor",
+  "MISMA" : "Marine ISM Auditor",
+  "MLCA" : "MLC Auditor",
+  "MM" : "Mooring Master ",
+  "OVI" : "Offshore Vessel Inspector",
+  "PICS" : "P& I Condition Surveyor",
+  "PI" : "Petroleum Inspector",
+  "PC" : "Port Captain",
+  "PVI" : "Pre- Vetting Inspector",
+  "RI" : "Right ship vetting Inspector",
+  "SIREVI" : "SIRE Vetting Inspector",
+  "VCI" : "Vessel Condition  Inspector",
+  "VCS" : "Vessel Construction Surveyor",
+  "VRS" : "Vessel Repair Superintendent",
+  "WCI" : "Warranty Claim Inspector"
 };
 
 global.vesselTypes = {
+  "ANCHOR": "Anchor Handler",
+  "BARGE" : "Barge",
+  "CAR": "Car Carrier Ship",
+  "CONTAINERSHIP": "Container Carrier" ,
+  "COT" : "Crude Oil Tanker",
+  "BULK_DRY": "Dry- Bulk Carrier " ,
   "LNG": "LNG Carrier" ,
   "LPG": "LPG Carrier" ,
+  "GEN_CAR": "MPV/General Cargo Ships" ,
   "CHEM_TANK": "Chemical Tanker" ,
-  "OIL_TANK": "Oil Tanker" ,
-  "PROD_TANK": "Products Tanker" ,
-  "BULK_CAR": "Bulk Carrier" ,
-  "OBO_CAR": "Ore Bulk Carrier (OBO)" ,
-  "SELF_DIS": "Self Discharging" ,
-  "BULK_DRY": "Bulk Dry" ,
-  "MPP": "Other Bulk - Multipurpose (MPP or MPV)" ,
-  "GEN_CAR": "General Cargo" ,
-  "P/G_CAR": "Passenger/General Cargo" ,
-  "CONTAINERSHIP": "Containership" ,
-  "REEFER": "Refrigerated (Reefer) Cargo Ship" ,
-  "CAR_RR_CAR": "Car Carrier / Ro-Ro Cargo" ,
-  "P_RR_CAR": "Passenger/Ro-Ro Cargo" ,
-  "PASS_SHIP": "Passenger Ship / Ferry / Cruise" ,
-  "OTH_DRY_CAR": "Other Dry Cargo" ,
-  "OSV_PSV": "Offshore/Platform Supply Vessel (OSV/PSV)" ,
-  "ANCHOR": "Anchor Handler" ,
-  "RES_VESSEL": "Research Vessel" ,
-  "TUG": "Tug" ,
-  "DREDGER": "Dredger" ,
-  "IN_WAT_TANK": "Inland Waterways Tanker" ,
-  "IN_WAT_DRY_CAR": "Inland Waterways Dry Cargo" ,
-  "NON_PROP": "Non Propelled" ,
-  "OIL_SPLILL_RV": "Oil Spill Recovery Vessels"
+  "OSV" : "Offshore Supply Vessel",
+  "OIL_TANK": "Oil/Chemical Tanker" ,
+  "P_SHIP": "Passenger Ships" ,
+  "PROD_TANK": "Products Tanker",
+  "R_CONT": "Reefer Container",
+  "RR_SHIP": "Ro-Ro Ships" ,
+  "TUG": "Tug Boats"
 };
 
 global.inspectorPositions = {
@@ -70,6 +90,97 @@ global.inspectorQualifications = {
   "OTHERS": "Others"
 };
 
+global.inspectorSkills = {
+  "CargoSurveying" : "Cargo Surveying ",
+  "CrudeOilWashing" : "Crude Oil Washing ",
+  "Dry-DockingProjectManagement" : "Dry-Docking Project Management ",
+  "EnvironmentalAudits" : "Environmental Audits",
+  "ISMAudtis" : "ISM Audtis",
+  "ISPS/MLCAudits" : "ISPS/MLC Audits",
+  "MarineCasualtyInvestigation" : "Marine Casualty Investigation ",
+  "MarineInspections" : "Marine Inspections ",
+  "MarineInsuranceClaims" : "Marine Insurance Claims",
+  "MarineRiskAnalysis" : "Marine Risk Analysis",
+  "MarineWarrantyClaims" : "Marine Warranty Claims ",
+  "MaritimeArbitrator" : "Maritime Arbitrator",
+  "NewBuildingSupervision" : "New Building Supervision",
+  "P&IConditionInspections" : "P& I Condition Inspections",
+  "PortCaptaincy" : "Port Captaincy ",
+  "Pre-PurchaseConditionInspection" : "Pre-Purchase  Condition Inspection ",
+  "PSCPreparationInspection" : "PSC Preparation Inspection",
+  "SIREPreparationInspection" : "SIRE Preparation Inspection",
+  "STSMooringOperation" : "STS Mooring Operation",
+  "VesselTechnicalInspection" : "Vessel Technical Inspection",
+  "WarrantyClaimInspection" : "Warranty Claim Inspection "
+};
+
+global.inspectorHighQualification = {
+  "BachelorinNauticalScience" : "Bachelor in Nautical Science",
+  "CharteredshipBroker" : "Chartered ship Broker",
+  "ChiefMarineEngineer" : "Chief Marine Engineer",
+  "MarineElectricalEngineer" : "Marine Electrical Engineer ",
+  "MarineEngineer" : "Marine Engineer",
+  "MarineMechanicalEngineer" : "Marine Mechanical Engineer",
+  "MaritimeLawyer" : "Maritime Lawyer ",
+  "MasterMariner" : "Master Mariner",
+  "NavalArchitect" : "Naval Architect",
+  "PostGraduateinEngineer" : "Post Graduate in Engineer",
+  "PostGraduateinNavalArchitect" : "Post Graduate in Naval Architect"
+};
+
+global.region = {
+  "ArabianGulf" : "Arabian Gulf ",
+  "Australia" : "Australia",
+  "BlackSeaArea" : "Black Sea Area",
+  "CentralAmerica" : "Central America",
+  "CentralEurope" : "Central Europe",
+  "ChinaOther" : "China - Other",
+  "ChinaSouth" : "China - South",
+  "ChinaCJK" : "China -CJK",
+  "ChinaNorth" : "China-- North",
+  "EastAfrica" : "East Africa",
+  "EastAsia" : "East Asia ",
+  "EastRussia" : "East Russia ",
+  "EasternEurope" : "Eastern Europe",
+  "FarEastAsia" : "Far East Asia",
+  "IndiaWest" : "India - West",
+  "IndiaEast" : "India East",
+  "IndiaSouth" : "India South",
+  "Mediterranean" : "Mediterranean ",
+  "NorthEastAfrica" : "North East Africa",
+  "NorthEastAmerica" : "North East America",
+  "NorthWestAfrica" : "North West Africa",
+  "NorthWestAmerica" : "North West America",
+  "NorthernEurope" : "Northern Europe",
+  "PersianGulf" : "Persian Gulf ",
+  "SouthAfrica" : "South Africa",
+  "SouthAmerica" : "South America",
+  "SouthEastAsia" : "South East Asia",
+  "WestAfrica" : "West Africa",
+  "WestRussia" : "West Russia",
+  "WesternEurope" : "Western Europe"
+};
+
+global.inspectorLevel = {
+  "CertificateCourse" : "Certificate Course ",
+  "Diploma" : "Diploma ",
+  "Graduate" : "Graduate",
+  "PostGraduate" : "Post Graduate",
+  "PostGraduateDiploma" : "Post Graduate Diploma",
+  "UnderGraduate" : "Under Graduate"
+};
+
+global.inspectorPositions = {
+  "FleetManager" : "Fleet Manager ",
+  "MarineAuditor" : "Marine Auditor",
+  "MarineSuperintendent" : "Marine Superintendent ",
+  "MarineSurveyor" : "Marine Surveyor ",
+  "NavalArchitect" : "Naval Architect ",
+  "ProjectManager" : "Project Manager",
+  "SiteManager" : "Site Manager",
+  "TechnicalSuperintendent" : "Technical Superintendent"
+};
+
 global.regionCodes = {
   "AF": "Africa",
   "AN": "Antarctica",
@@ -88,7 +199,7 @@ global.mysqlConfig = {
 };
 
 global.appConfig = {
-  emailTemplatesPath : 'C:/Users/Vivek/Desktop/web/service/emailtemplates',
+  emailTemplatesPath : 'D:/personnel/shipinspector/web/service/emailtemplates/',
   accessKeyId: 'AKIAIFENAUAFOPXUTNQQ',
   secretAccessKey: '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',
   defaultFrom: 'Team ShipInspector <noreply@shipinspectors.com>',
