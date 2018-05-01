709ebe5f148cf0fe6fcc4b84c7bc4defb5f5deb6
Vivek Patel <Vivek Patel>
2018-05-01 20:26:16 +0530

    admin-user orders

---
+++ client/containers/AdminOrderContainer.js
@@ -0,0 +1,20 @@
+
+import { connect } from 'react-redux'
+import AdminOrderComponent from '../components/admin/AdminOrderComponent.jsx';
+
+import {getAdminOrders} from '../actions/order';
+
+const mapStateToProps = (state) => {
+	const { userProfile, error, orders } = state;
+  	return {userProfile, error, orders };
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {	
+        getAdminOrders: () => {
+        dispatch(getAdminOrders());
+      }
+	};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AdminOrderComponent);
