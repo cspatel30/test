a369df5b3679e23ac6920396166fc01ea560cf33
Vivek Patel <Vivek Patel>
2018-06-14 11:22:24 +0530

    header footer

--- client/components/homePage/Footer.js
+++ client/components/homePage/Footer.js
@@ -42,7 +42,16 @@ const Footer = () => {
           {renderQuickLinks('right')}
         </div>
       </div>
-      <div className="col-4 right-section">3</div>
+      <div className="col-4 px-5 right-section d-flex flex-column">
+        <b className="mb-3">CONNECT WITH US</b>
+        <div className="mb-3"><i className="fa fa-twitter mr-2" /><span className="item-opa">FOLLOW US ON TWITTER</span></div>
+        <div className="mb-5"><i className="fa fa-linkedin mr-2" /><span className="item-opa">JOIN US ON LINKEDIN</span></div>
+        <span className="hr-right mb-5" />
+        <b className="mb-4">PAY ONLINE</b>
+        <div className="credit-card-img">
+          <a href="http://www.credit-card-logos.com/"><img alt="Credit Card Logos" title="Credit Card Logos" src="http://www.credit-card-logos.com/images/multiple_credit-card-logos-1/credit_card_logos_17.gif" width="235" height="35" border="0" /></a>
+        </div>
+      </div>
     </div>
   );
 }
