a29daa9b58237a3f4996fc0ac432f34129bf7a9a
richa@repozitory.com <richa@repozitory.com>
2018-06-15 17:28:53 +0530

    Included Header and Footer

--- client/components/MyAccountPage.jsx
+++ client/components/MyAccountPage.jsx
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
-import { Switch, Route, Redirect, HashRouter } from 'react-router-dom';
-import CustomerBillingContainer from '../containers/CustomerBillingContainer';
-import CustomerSettingContainer from '../containers/CustomerSettingContainer';
-import CustomerProfileContainer from '../containers/CustomerProfileContainer';
-
-import Header from './myAccount/Header';
+import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
+import CustomerBillingContainer from "../containers/CustomerBillingContainer";
+import CustomerSettingContainer from "../containers/CustomerSettingContainer";
+import CustomerProfileContainer from "../containers/CustomerProfileContainer";
+import MainHeader from "./homePage/Header";
+import Footer from "./homePage/Footer";
+import Header from "./myAccount/Header";
 
 class MyAccount extends Component {
-
   render() {
-    console.log(this.props.match)
     return (
       <div>
+        <MainHeader />
         <Header {...this.props} />
         <div>
-          <CustomerProfileContainer id="profile"/>
+          <CustomerProfileContainer id="profile" />
           <div className="shadowDivider" id="billing" />
           <CustomerBillingContainer id="billing" />
           <div className="shadowDivider" />
-          <CustomerSettingContainer id="setting" />  
+          <CustomerSettingContainer id="setting" />
         </div>
+        <Footer />
       </div>
     );
   }
