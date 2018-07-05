bfae11c57047b4e68da602136ee7a41c4666ec90
suresh_borad <kusum.ratawa@bacancytechnology.com>
2018-07-05 13:19:01 +0530

    api path

--- client/actions/auth2.js
+++ client/actions/auth2.js
@@ -1,7 +1,7 @@
 import Request from 'axios';
 import { TOKEN_VERIFIED } from '../constants/ActionsTypes';
 
-const ip = '/api';
+const ip = 'http://sis-beta.us-east-1.elasticbeanstalk.com';
 function makeRequest(method, api = '/login', data) {
   return Request[method](ip + api, data)
         .then(r => r);
